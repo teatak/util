4ee5a25e58a0d59a383835e0ccd01f06b2a61314
Yang <yangglive@icloud.com>
2022-11-02 09:42:49 +0800

    change seg

--- common/seg.go
+++ common/seg.go
@@ -4,9 +4,15 @@ import (
 	"github.com/go-ego/gse"
 )
 
-var seg, _ = gse.New("./data/dict/zh/s_1.txt,./data/dict/zh/user.txt")
+var seg *gse.Segmenter
 
 func Segment(keywords string) []string {
+
+	if seg == nil {
+		_seg, _ := gse.New("./data/dict/zh/s_1.txt,./data/dict/zh/user.txt")
+		seg = &_seg
+	}
+	//
 	//删除重复
 	allKeys := make(map[string]bool)
 	list := []string{}
